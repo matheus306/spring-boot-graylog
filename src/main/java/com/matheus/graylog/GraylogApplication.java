cbb303b2faaf78dcefaa0a9edcdedf56e6baf153
matheus306 <Ma010203@>
2018-09-25 22:18:31 -0300

    Primeiro Commit

---
+++ src/main/java/com/matheus/graylog/GraylogApplication.java
@@ -0,0 +1,12 @@
+package com.matheus.graylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraylogApplication.class, args);
+    }
+}
